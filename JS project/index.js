5359a5c81ba72fa9806ff38e10b2d085c6d7f53c
Madan Mohan Behera <132214432+Madan1500@users.noreply.github.com>
2023-11-25 22:47:21 +0530

    Add files via upload

--- JS project/index.js
+++ JS project/index.js
@@ -1,7 +1,7 @@
 let input=document.querySelector(`input`);
 let innerBox=document.querySelector(".inner-box");
 let outerBox=document.querySelector(".outer-box");
-
+let mode=document.querySelector(`.mode`);
 input.style.fontSize="1.25rem";
 const clean=()=>{input.value = ' ';}
 const display=(value)=>{input.value+=value;}
@@ -21,6 +21,8 @@ const invert=()=>{
         Body.style.outlineColor="black";
         Button.style.backgroundColor="black";
         Button.style.color="white";
+        mode.innerHTML=`<span class="material-symbols-outlined">dark_mode</span>`;
+        Button.prepend(mode);
         innerBox.style.backgroundColor="green";
         outerBox.style.backgroundColor="blue";
     }
@@ -28,6 +30,8 @@ const invert=()=>{
         Body.style.backgroundColor="black";
         Body.style.outlineColor="white";
         Button.style.backgroundColor="white";
+        mode.innerHTML=`<span class="material-symbols-outlined">light_mode</span>`;
+        Button.prepend(mode);
         Button.style.color="red";
         innerBox.style.backgroundColor="blue";
         outerBox.style.backgroundColor="rgb(218,197,197)";
